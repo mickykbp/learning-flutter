6149b48b7ed01e9465ca760185e75855b33d5c77
mickykbp <3651051541120@rmutr.ac.th>
2024-12-02 12:16:48 +0700

    second

--- flutter_sample/lib/main.dart
+++ flutter_sample/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample/my_container.dart';
+import 'package:flutter_sample/my_scroll.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,11 +14,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'My Flutter Sample',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+      home: MyScrllView()
+    ) ;
   }
 }
